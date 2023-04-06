98f05bec07658d21ab033f8f66c2e5cbc66e18ea
Marie Kagermanova <maria.kgm04@gmail.com>
2023-04-06 11:05:05 +0300

    feat: add support for cyrillic letters

--- src/share/constants.ts
+++ src/share/constants.ts
@@ -1,7 +1,7 @@
 import { TagItem } from "./interfaces";
 
 export const notesStorage = "notes";
-export const tagRegex: RegExp = /#\w+/gi;
+export const tagRegex: RegExp = /#[ЁёА-я\w]+/gi;
 export const convertToHtml = (arr: string[]): string => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "#" && arr[i + 1] !== " " && arr[i - 1] !== ">") {
